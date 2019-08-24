7b9f3e5fb13ad694553ede140874329e8a9a4855
jasonofua <jasonofua@gmail.com>
2019-08-24 10:12:31 +0100

    new

--- public/newfile.php
+++ public/newfile.php
@@ -9,19 +9,9 @@ $text        = $_POST["text"];
 $textValue   = explode ("*", $text);
 $level = count($textValue);
 
-if ($text == "") {
-    // This is the first request. Note how we start the response with CON
-    $response  = "CON What would you want to do \n";
-    $response .= "1. Verify Pin \n";
-    $response .= "2. Register \n";
-    $response .= "3. Account Balance \n";
-    $response .= "4. Make Deposit \n";
-    $response .= "5. Transfer \n";
-    $response .= "6. Withdraw fund \n";
-    $response .= "7. Buy Pin ";
 
-} 
+
 
 // Echo the response back to the API
 header('Content-type: text/plain');
-echo $response;
+echo $level;
