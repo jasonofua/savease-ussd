4454dcb3ac577c043a7c697916175854105c3772
jasonofua <jasonofua@gmail.com>
2019-08-24 13:50:27 +0100

    testingapis

--- public/index.php
+++ public/index.php
@@ -1,27 +1,6 @@
 <?php
 require_once('../lib/nusoap.php');
 
-// Reads the variables sent via POST from our gateway
-$sessionId   = $_POST["sessionId"];
-$serviceCode = $_POST["serviceCode"];
-$phoneNumber = $_POST["phoneNumber"];
-$text        = $_POST["text"];
-$str_arr = explode ("*", $text);
-$num = count($str_arr);
-$userResponse=trim(end($str_arr));
-
-if ($text == ""){
-    $response  = "CON What would you want to do \n";
-    $response .= "1. Verify Voucher \n";
-    $response .= "2. My phone number";
-}else if($text == "1"){
-    $response  = "CON Enter Voucher Pin".$userResponse;
-
-    
-}else if($num == 2){
-
-if($str_arr[0] == "1"){
-
     $wsdl   = "http://savease.ng/webservice1.asmx?wsdl";
         $client = new nusoap_client($wsdl, 'wsdl');
 
@@ -40,20 +19,18 @@ if($str_arr[0] == "1"){
 
 	if (isset($action))
 	{
-		$result['response'] = $client->call($action, $json);
+		$result['response'] = $client->call($action, "");
 	}
 
         $response = "".$result['response']['VerifyPinResult'];
    
-}
-    
-    
-}
 
 
-
-// Echo the response back to the API
-header('Content-type: text/plain');
-echo $response;
+    echo "<h3>Output : </h3>";
+	echo $result['response'];
+	echo "<h2>Request</h2>";
+	echo "<pre>" . htmlspecialchars($client->request, ENT_QUOTES) . "</pre>";
+	echo "<h2>Response</h2>";
+	echo "<pre>" . htmlspecialchars($client->response, ENT_QUOTES) . "</pre>";
 
 ?>
