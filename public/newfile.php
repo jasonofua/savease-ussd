8a6207ad2fd977732946ba8cfb222d6112576750
jasonofua <jasonofua@gmail.com>
2019-08-24 10:20:26 +0100

    new code

--- public/newfile.php
+++ public/newfile.php
@@ -1,17 +1,8 @@
 <?php
 // Reads the variables sent via POST from our gateway
     $config = new SoapClient("http://savease.ng/webservice1.asmx?wsdl");
-
-$sessionId   = $_POST["sessionId"];
-$serviceCode = $_POST["serviceCode"];
-$phoneNumber = $_POST["phoneNumber"];
-$text        = $_POST["text"];
-$textValue   = explode ("*", $text);
-$level = count($textValue);
+    $result = $config->__soapCall('VerifyPin', array('inputParame' => "1234567898765"));
+    var_dump($result);
 
 
 
-
-// Echo the response back to the API
-header('Content-type: text/plain');
-echo $level;
