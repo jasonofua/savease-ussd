86faeb75a15f29dc1ccae3c65fc36160ee6010b3
jasonofua <jasonofua@gmail.com>
2019-08-24 11:15:00 +0100

    changed library

--- public/index.php
+++ public/index.php
@@ -1,8 +1,37 @@
 <?php
+require_once('lib/nusoap.php');
+
+
+$wsdl   = "http://savease.ng/webservice1.asmx?wsdl";
+$client = new nusoap_client($wsdl, 'wsdl');
+
+$error = $client->getError();
+
+$json	  = '{"inputParame":"1234567898765"}';
+	
+	if ($error)
+	{
+		echo $error; die();
+	}
+	
+    $action = "VerifyPin";
+    
+    $result = array();
+
+	if (isset($action))
+	{
+		$result['response'] = $client->call($action, "");
+	}
 // Reads the variables sent via POST from our gateway
-    $config = new SoapClient("http://savease.ng/webservice1.asmx?wsdl");
-    $result = $config->__soapCall('VerifyPin', array('inputParame' => "1234567898765"));
-    var_dump($result);
+    var_dump($result['response']);
 
 
+    echo "<h3>Output : </h3>";
+	echo $result['response'];
+	echo "<h2>Request</h2>";
+	echo "<pre>" . htmlspecialchars($client->request, ENT_QUOTES) . "</pre>";
+	echo "<h2>Response</h2>";
+	echo "<pre>" . htmlspecialchars($client->response, ENT_QUOTES) . "</pre>";
+
+    ?>
 
