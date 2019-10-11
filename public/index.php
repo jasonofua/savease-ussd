2def6fa0387cc6da3b0151690e77ace4d9290483
jasonofua <jasonofua@gmail.com>
2019-10-11 22:08:29 +0100

    added sms test issues fix tested

--- public/index.php
+++ public/index.php
@@ -79,23 +79,20 @@ if($str_arr[0] == "1"){
         'body' => 'Your Acct '.$newString.' Has Been Credited  By SAVEASE DEPOSIT - (Transaction Ref)CR',
         'dnd'=> '2'
 );
-// curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-// curl_setopt($curl, CURLOPT_POST, true);
-// curl_setopt($curl, CURLOPT_POSTFIELDS, $curl_post_data);
-// $curl_response = curl_exec($curl);
-// if ($curl_response === false) {
-//     $info = curl_getinfo($curl);
-//     curl_close($curl);
-//     die('error occured during curl exec. Additioanl info: ' . var_export($info));
-// }
-// curl_close($curl);
-// $decoded = json_decode($curl_response);
-// if (isset($decoded->response->status) && $decoded->response->status == 'ERROR') {
-//     die('error occured: ' . $decoded->response->errormessage);
-// }
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $curl_post_data);
+$curl_response = curl_exec($curl);
+if ($curl_response === false) {
+    $info = curl_getinfo($curl);
+    curl_close($curl);
+    die('error occured during curl exec. Additioanl info: ' . var_export($info));
+}
+curl_close($curl);
+$decoded = json_decode($curl_response);
 
-      
-        $response = "END Your deposit was successful. ".$curl_post_data[3];
+
+        $response = "END Your deposit was successful. ".$decoded;
     
          
     }else{
